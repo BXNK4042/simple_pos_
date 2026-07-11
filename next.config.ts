75c70c32513ccd5acf1469bbe320e140f505b17e
BXNK4042 <parinthonkongthong@gmail.com>
2026-07-11 21:29:19 +0700

    config: add 10.62.123.61 to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["172.16.15.93"],
+  allowedDevOrigins: ["172.16.15.93", "10.62.123.61"],
 };
 
 export default nextConfig;
