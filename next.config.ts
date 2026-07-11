3cbded5800187c69e1313e53f9ab239566887f2b
BXNK4042 <parinthonkongthong@gmail.com>
2026-07-11 21:41:04 +0700

    config: add dev origin 10.123.182.61, clean site-top-bar unused brand link

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["172.16.15.93", "10.62.123.61"],
+  allowedDevOrigins: ["172.16.15.93", "10.62.123.61", "10.123.182.61"],
 };
 
 export default nextConfig;
